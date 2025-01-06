06f43000ba5c63563ccefb0b2458cb0bb6de86b6
AinzleKhoa <khoale1855@gmail.com>
2025-01-06 17:03:10 +0700

    The initial project WED
    a project from WED to test how github works

---
+++ mycode.js
@@ -0,0 +1,9 @@
+function showTable() {
+    var table = document.getElementById('tableid');
+
+    if (table.style.display == 'block') {
+        table.style.display = 'none';
+    } else {
+        table.style.display = 'block';
+    }
+}
